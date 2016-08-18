b64c21b53adffd8a6cc7b59936eda0d234259973
ALavruhin <mdvd4@mail.ru>
2016-08-18 09:56:16 +0300

    Добалены функции: приветствия, вычисления площади квадрата и прямоугольника

--- sandbox/src/main/java/lavr/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/lavr/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,28 @@ package lavr.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Frodo Baggins");
+
+    double l = 6;
+    System.out.println("площадь квадрата со стороной " + l + " равна " + area(l));
+
+    double a = 3;
+    double b = 4;
+    System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
