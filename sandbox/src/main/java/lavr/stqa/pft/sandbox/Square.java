d94077c611ddc7600896dfb41965490c23b80091
ALavruhin <mdvd4@mail.ru>
2016-08-18 10:30:28 +0300

    Создан класс Квадрат, определён метод площадь

---
+++ sandbox/src/main/java/lavr/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package lavr.stqa.pft.sandbox;
+
+/**
+ * Created by Lavr on 18.08.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
