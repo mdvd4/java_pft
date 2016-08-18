137c618d3ce2e29f0d57e936b65da8117485ba02
ALavruhin <mdvd4@mail.ru>
2016-08-18 13:28:52 +0300

    Создан класс Point для представления точек на плоскости.

---
+++ sandbox/src/main/java/lavr/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package lavr.stqa.pft.sandbox;
+
+/**
+ * Created by Lavr on 18.08.2016.
+ * Класс описывающий точку на плоскости
+ */
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point (double x, double y) {
+    this.x=x;
+    this.y=y;
+  }
+}
