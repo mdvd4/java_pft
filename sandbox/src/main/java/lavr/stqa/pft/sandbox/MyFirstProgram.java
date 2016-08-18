e0fbfabeff64a75fb4853ec84bad8abeeb18702e
ALavruhin <mdvd4@mail.ru>
2016-08-18 16:04:48 +0300

    Исключена перая версия  расчёта дистанции  между точками из MyFirstProgram

--- sandbox/src/main/java/lavr/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/lavr/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,12 +4,6 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    Point point1 = new Point(1, 0);
-    Point point2 = new Point(2, 0);
-    Point point3 = new Point(-1, 0);
-    distance(point1, point2);
-    distance(point2, point3);
-
     hello("world");
 
     Square s = new Square(6);
@@ -20,12 +14,6 @@ public class MyFirstProgram {
 
   }
 
-  public static void distance(Point p1, Point p2) {
-    /** Функция вычисляющая дистанцию между двумя точками*/
-    String text1 = "растояние между точками [" + p1.x + "," + p1.y + "], [" + p2.x + "," + p2.y + "] равно ";
-    double distanse = Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
-    System.out.println(text1 + distanse);
-  }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
