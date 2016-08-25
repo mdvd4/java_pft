b579a7aff6ceea5edbaba8a9fc9bdad16359c988
ALavruhin <mdvd4@mail.ru>
2016-08-25 15:16:23 +0300

    Создан специализированный класс помощник SessionHelper

--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/appmanager/ApplicationMenager.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/appmanager/ApplicationMenager.java
@@ -12,6 +12,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationMenager {
   FirefoxDriver wd;
+
+  private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
@@ -30,21 +32,12 @@ public class ApplicationMenager {
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
   public void stop() {
     wd.quit();
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[3]")).click();
   }
 
   public void returnToHomePage() {
