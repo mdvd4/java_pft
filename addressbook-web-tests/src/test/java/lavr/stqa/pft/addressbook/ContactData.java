c00f347b588df7c0c612eb56ca3bfbc74f763cf6
ALavruhin <mdvd4@mail.ru>
2016-08-25 13:13:11 +0300

    Разработан тест для создания нового контакта ContactCreationTests
    Записан тест в Selenium Builder для создания  контакта и перенесён в
    среду разработки.
    Выделены из кода теста вспомогательные методы initContactCreation(),
    fillContactForm(), submitContactCreation(), returnToHomePage().
    Параметризован метод заполнения формы fillContactForm().
    Создан вспомогательный "модельный" класс ContactData для представления
    объекта, группирующего параметры метода fillContactForm

---
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package lavr.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String fhonehome;
+  private final String fhonemobile;
+  private final String fonework;
+  private final String fhonefax;
+  private final String emal;
+  private final String email2;
+  private final String email3;
+
+  public ContactData(String firstname, String lastname, String address, String fhonehome, String fhonemobile, String fonework, String fhonefax, String emal, String email2, String email3) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.fhonehome = fhonehome;
+    this.fhonemobile = fhonemobile;
+    this.fonework = fonework;
+    this.fhonefax = fhonefax;
+    this.emal = emal;
+    this.email2 = email2;
+    this.email3 = email3;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getFhonehome() {
+    return fhonehome;
+  }
+
+  public String getFhonemobile() {
+    return fhonemobile;
+  }
+
+  public String getFonework() {
+    return fonework;
+  }
+
+  public String getFhonefax() {
+    return fhonefax;
+  }
+
+  public String getEmal() {
+    return emal;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+}
