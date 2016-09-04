8c2c92244d74b29056eba9f72214b604fdbc113c
ALavruhin <mdvd4@mail.ru>
2016-09-04 15:29:00 +0300

    Оптимизация тестов: "Ленивые ссылки" для методов gotoGroupPage, gotoHomePage, добавлен вспомогательный метод isElementPresent

--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,6 +1,7 @@
 package lavr.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -15,10 +16,18 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Group")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
+      click(By.linkText("groups"));
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 
