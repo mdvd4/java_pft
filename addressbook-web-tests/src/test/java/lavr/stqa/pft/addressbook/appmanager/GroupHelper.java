a5304cdd052e2756c055a35bd29bd04efc4f8578
ALavruhin <mdvd4@mail.ru>
2016-09-04 20:26:05 +0300

    Получение списка Групп

--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -3,6 +3,10 @@ package lavr.stqa.pft.addressbook.appmanager;
 import lavr.stqa.pft.addressbook.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Lavr on 25.08.2016.
@@ -60,5 +64,17 @@ public class GroupHelper extends HelperBase {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> GetGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement elemet :elements){
+      String name = elemet.getText();
+      GroupData group =new GroupData(name,null,null);
+      groups.add(group);
+    }
+
+    return groups;
   }
 }
--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,19 +4,23 @@ import lavr.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().GetGroupList();
+    //int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", null));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after,before+1);
+    List<GroupData> after = app.getGroupHelper().GetGroupList();
+    //int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(),before.size()+1);
   }
 
 }
