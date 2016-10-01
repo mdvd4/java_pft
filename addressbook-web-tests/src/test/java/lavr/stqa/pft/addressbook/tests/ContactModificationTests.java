d033bbc187ac674c71e14badc7172d96495682d1
ALavruhin <mdvd4@mail.ru>
2016-10-01 15:38:37 +0300

    Рефакторинг теста ContactModificationTests
    Модифицируем последний контакт в списке контактов, раньше модифицировали
    первый

--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -19,16 +19,16 @@ public class ContactModificationTests extends TestBase {
       app.getContactHelper().createContact(new ContactData("John", "Smith2", "[none]", "www.leningrad.spb.ru", "123-34-45-01", null, null, "123-34-45-03", "john@mail.com", null, null));
     }
     List<ContactData> before = app.getContactHelper().GetContactList();
-    app.getContactHelper().editContact();
-    // попоробовать исправитьть добавить ID
-    ContactData contact = new ContactData("John", "Smith_mod1", null, null, "123-34-45-99", "123-34-45-02", null, null, null, "john@mail2.com", null);
+    int index = before.size() - 1;
+    ContactData contact = new ContactData(before.get(index).getId(),"John", "Smith_mod1", null, null, "123-34-45-99", "123-34-45-02", null, null, null, "john@mail2.com", null);
+    app.getContactHelper().editContact(index);
     app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().GetContactList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(0); //редактируем всегда первый контакт
+    before.remove((before.size() - 1)); //редактируем последний контакт
     before.add(contact);
     Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
