0c2a182763bda1b02356992adb5fdebc43c5115c
ALavruhin <mdvd4@mail.ru>
2016-10-01 12:40:31 +0300

    Реализована проверка путём сравнения списков в тесте для модификации контактов.

--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,11 @@
 package lavr.stqa.pft.addressbook.tests;
 
 import lavr.stqa.pft.addressbook.model.ContactData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by Lavr on 04.09.2016.
@@ -14,10 +18,21 @@ public class ContactModificationTests extends TestBase {
     if (!app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("John", "Smith2", "[none]", "www.leningrad.spb.ru", "123-34-45-01", null, null, "123-34-45-03", "john@mail.com", null, null));
     }
+    List<ContactData> before = app.getContactHelper().GetContactList();
     app.getContactHelper().editContact();
-    app.getContactHelper().fillContactForm(new ContactData("John", "Smith_mod1", null, null, "123-34-45-99", "123-34-45-02", null, null, null, "john@mail2.com", null), false);
+    ContactData contact = new ContactData("John", "Smith_mod1", null, null, "123-34-45-99", "123-34-45-02", null, null, null, "john@mail2.com", null);
+    app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().GetContactList();
+    Assert.assertEquals(after.size(), before.size());
+
+    before.remove(0); //редактируем всегда первый контакт
+    before.add(contact);
+    Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
