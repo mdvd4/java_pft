f6ac7693cd17bb0b66437c54b792815ef1ac64fd
ALavruhin <mdvd4@mail.ru>
2016-10-01 12:04:11 +0300

    Реализована  проверки путём сравнения списков в тестах для создания контактов

--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/model/ContactData.java
@@ -1,6 +1,7 @@
 package lavr.stqa.pft.addressbook.model;
 
 public class ContactData {
+  private int id;
   private final String firstname;
   private final String lastname;
   private String group;
@@ -13,7 +14,23 @@ public class ContactData {
   private final String email2;
   private final String email3;
 
+  public ContactData(int id, String firstname, String lastname, String group, String address, String fhonehome, String fhonemobile, String fonework, String fhonefax, String emal, String email2, String email3) {
+    this.id = id;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.group = group;
+    this.address = address;
+    this.fhonehome = fhonehome;
+    this.fhonemobile = fhonemobile;
+    this.fonework = fonework;
+    this.fhonefax = fhonefax;
+    this.email = emal;
+    this.email2 = email2;
+    this.email3 = email3;
+  }
+
   public ContactData(String firstname, String lastname, String group, String address, String fhonehome, String fhonemobile, String fonework, String fhonefax, String emal, String email2, String email3) {
+    this.id = Integer.MAX_VALUE;
     this.firstname = firstname;
     this.lastname = lastname;
     this.group = group;
@@ -69,5 +86,37 @@ public class ContactData {
 
   public String getGroup() {
     return group;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "id=" + id +
+            ", firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
   }
 }
--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,16 +1,32 @@
 package lavr.stqa.pft.addressbook.tests;
 
 import lavr.stqa.pft.addressbook.model.ContactData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
+    app.getNavigationHelper().gotoHomePage();
+    List<ContactData> before = app.getContactHelper().GetContactList();
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("John", "Smith", "[none]", "www.leningrad.spb.ru", "123-34-45-01", null, null, "123-34-45-03", "john@mail.com", null, null), true);
+    ContactData contact = new ContactData("John", "Smith2", "[none]", "www.leningrad.spb.ru", "123-34-45-01", null, null, "123-34-45-03", "john@mail.com", null, null);
+    app.getContactHelper().fillContactForm(contact, true);
     app.getContactHelper().submitContactCreation();
     app.getContactHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().GetContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(contact);
+    Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
+
   }
 
 }
