c7da1ffd780baccbf8e1ed5d5a6e0f98a281d351
ALavruhin <mdvd4@mail.ru>
2016-10-01 12:52:47 +0300

    Реализована проверка путём сравнения списков в тестах  удаления контактов.

--- addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactDelitionTests.java
+++ addressbook-web-tests/src/test/java/lavr/stqa/pft/addressbook/tests/ContactDelitionTests.java
@@ -1,7 +1,10 @@
 package lavr.stqa.pft.addressbook.tests;
 
 import lavr.stqa.pft.addressbook.model.ContactData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 /**
  * Created by Lavr on 25.08.2016.
@@ -13,9 +16,15 @@ public class ContactDelitionTests extends TestBase {
     if (!app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("John", "Smith", "[none]", "www.leningrad.spb.ru", "123-34-45-01", null, null, "123-34-45-03", "john@mail.com", null, null));
     }
+    List<ContactData> before = app.getContactHelper().GetContactList();
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContact();
     app.getNavigationHelper().gotoHomePage();
+    List<ContactData> after = app.getContactHelper().GetContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 }
